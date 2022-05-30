9654ae91348fe48ddbaba5db10c9a9244165a102
Tiamba Alucho Henry Anthony <51093150+TIAMBAALUCHOHENRYANTHONY@users.noreply.github.com>
2022-05-30 18:33:30 -0500

    Updated

---
+++ Project (2)/Project/addMedicine.php
@@ -0,0 +1,15 @@
+<?php
+	require_once("connectMedicine.php");
+	$Name = $_POST["nameMedicine"];
+	$Id = $_POST["idMedicine"];
+	$Description = $_POST["descriptionMedicine"];
+	$Price = $_POST["priceMedicine"];
+	$Quantity = $_POST["quantityMedicine"];
+	$ElabDate = $_POST["ElaborationDateMedicine"];
+	$ExpDate = $_POST["ExpirationDateMedicine"];
+	
+	$newMedicine = array("Name"=>$Name,"Id"=>$Id,"Description"=>$Description,"Price"=>$Price,"Quantity"=>$Quantity,"ElaborationDate"=>$ElabDate,"ExpirationDate"=>$ExpDate);
+	$medicines->insertOne($newMedicine);
+
+	header("Refresh: 0;url=main.php?mensaje=2")
+?>
