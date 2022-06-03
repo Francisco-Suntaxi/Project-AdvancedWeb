33c0a43c867b55b05bb935072aa237ebbffa3956
Francisco-Suntaxi <sfsuntaxi1@espe.edu.ec>
2022-06-02 23:42:57 -0500

    delete project_E

--- Project_Farmaveci_edison/connectMedicine.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require 'vendor/autoload.php';
-$connection = new MongoDB\Client;
-$mongo = new MongoDB\Client('mongodb+srv://admin:admin@cluster0.pukdr.mongodb.net/?retryWrites=true&w=majority');
-$db = $mongo->farmaciadb;//database
-$medicines = $db->medicinas;//collection
-?>
