5282007985b4188c35b4b1f29fafe20affe2463c
Francisco-Suntaxi <sfsuntaxi1@espe.edu.ec>
2022-07-12 23:28:30 -0500

    Folders

--- Project CS Web Application/Project_Farmaveci/head.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<title>Farmaveci</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="UTF-8">
-
-<link href="css/bootstrap.min.css" rel="stylesheet"/>
-
-<link href="css/bootstrap-responsive.min.css" rel="stylesheet"/>
