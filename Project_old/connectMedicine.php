e596bab3196af2d18b462661d2a4faa98a16f6ec
Francisco-Suntaxi <stalynfran@hotmail.com>
2022-07-16 23:08:22 -0500

    old

--- Project_old/connectMedicine.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require 'vendor/autoload.php';
-$connection = new MongoDB\Client;
-$mongo = new MongoDB\Client('mongodb://localhost:27017');
-$db = $mongo->farmaciadb;//database
-$medicines = $db->medicinas;//collection
-?>
