a306871a2634648eec74c56629bf5ca84beaa1d9
Andrés Gerardo Valarezo Correa <84350542+AndresValarezo@users.noreply.github.com>
2022-08-17 18:34:53 -0500

    Expiration Medicines

--- 06-Code/Farmaveci_React/server/routes/routes.js
+++ 06-Code/Farmaveci_React/server/routes/routes.js
@@ -28,7 +28,7 @@ router.put('/quantity/:id',editMedicineQuantity);// Bussines Uri
 
 //Delete methods
 router.delete('/:id',deleteMedicine);
-router.delete('/expDate/actualDate',deleteMedicineActualDate);//Bussines Uri
+router.delete('/expDate/:actualDate',deleteMedicineActualDate);//Bussines Uri
 router.delete('/category/:category',deleteMedicineCategory);
 router.delete('/user/:userName',deleteUser);
 
